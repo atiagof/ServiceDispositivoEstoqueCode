e7fea41dec5706ff2ecab8bcdf8830943bbdc0e1
Alex Ferreira <alex.ferreira@yaman.com.br>
2018-05-29 16:28:02 -0300

    Primeiro commit

---
+++ serviceStandard/serviceStandard/IService1.cs
@@ -0,0 +1,114 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace serviceStandard
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da interface "IService1" no arquivo de código e configuração ao mesmo tempo.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Adicione suas operações de serviço aqui
+        [OperationContract]
+        Dispositvo GetDispositivoEstoque(double numDispositvo);
+
+    }
+
+    // Use um contrato de dados como ilustrado no exemplo abaixo para adicionar tipos compostos a operações de serviço.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    // Use um contrato de dados como ilustrado no exemplo abaixo para adicionar tipos compostos a operações de serviço.
+    [DataContract]
+    public class Dispositvo
+    {
+        string codigoEmpresa;
+        string codigoPosto;
+        string codigoProduto;
+        string sequencial;
+        string tipo;
+        string statusValor;
+        string mensagem;
+        string tipoChamada;
+
+        [DataMember]
+        public string CodigoEmpresa
+        {
+            get { return codigoEmpresa; }
+            set { codigoEmpresa = value; }
+        }
+
+        [DataMember]
+        public string CodigoPosto
+        {
+            get { return codigoPosto; }
+            set { codigoPosto = value; }
+        }
+
+        [DataMember]
+        public string CodigoProduto
+        {
+            get { return codigoProduto; }
+            set { codigoProduto = value; }
+        }
+
+        [DataMember]
+        public string Sequencial
+        {
+            get { return sequencial; }
+            set { sequencial = value; }
+        }
+
+        [DataMember]
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        [DataMember]
+        public string Status
+        {
+            get { return statusValor; }
+            set { statusValor = value; }
+        }
+
+        [DataMember]
+        public string Mensagem
+        {
+            get { return mensagem; }
+            set { mensagem = value; }
+        }
+
+        [DataMember]
+        public string TipoChamada
+        {
+            get { return tipoChamada; }
+            set { tipoChamada = value; }
+        }
+    }
+}
---
+++ serviceStandard/serviceStandard/ServiceEstoqueDaf.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace serviceStandard
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Service1" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Service1.svc ou Service1.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class ServiceEstoqueDaf : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Dispositvo GetDispositivoEstoque(double numDispositvo)
+        {
+            var Num_Sequencial = (numDispositvo + Convert.ToDouble(1));
+
+            var ObjDispositivo = new Dispositvo
+            {
+                CodigoEmpresa = "1",
+                CodigoPosto = "40142",
+                CodigoProduto = "008",
+                Sequencial = Num_Sequencial.ToString(),
+                Tipo = "Comodato",
+                Status = "Válido",
+                Mensagem = "Dispositivo Válido",
+                TipoChamada = "Solicitacao"
+            };
+
+            return ObjDispositivo;
+        }
+    }
+}
